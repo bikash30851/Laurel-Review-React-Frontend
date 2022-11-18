f0052fc3b0806866d695e6679990f9231da90a6d
Bikash Adhikari <bikash30851@gmail.com>
2022-11-18 09:31:35 -0600

    removed image

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -50,11 +50,11 @@ function Login() {
                 <div class="container py-5 h-100">
                     <div class="row d-flex align-items-center justify-content-center h-100">
                         <div class="col-md-8 col-lg-7 col-xl-6">
-                            <img
+                            {/* <img
                                 src="https://laurelreview.org/uploads/issues/fpvXBRTrJQ53WQqT_461.jpeg"
                                 className="img-fluid"
                                 alt="Phone"
-                            ></img>
+                            ></img> */}
                         </div>
                         <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
                             <form>
